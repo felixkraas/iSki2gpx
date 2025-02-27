c9bb5892cfab633c0b7e278c6132f73c0fef12b6
felix <felix@kraas-family.de>
2025-02-27 13:31:15 +0100

    Add GPX schema validation and metadata attributes

--- src/iSki2gpx.Converter/GPX/Models/GpxElement.cs
+++ src/iSki2gpx.Converter/GPX/Models/GpxElement.cs
@@ -1,7 +1,8 @@
+using System.Xml.Schema;
 using System.Xml.Serialization;
 
 namespace iSki2gpx.Converter.GPX.Models {
-    [XmlRoot( ElementName = "gpx", Namespace = "http://www.topografix.com/GPX/1/1", IsNullable = false )]
+    [XmlRoot( ElementName = "gpx", IsNullable = false )]
     public class GpxElement {
         [XmlElement( ElementName = "metadata" )]
         public GpxMetadata Metadata {
@@ -13,6 +14,18 @@ namespace iSki2gpx.Converter.GPX.Models {
         public GpxTrack Track {
             get;
             set;
+        }
+
+        [XmlAttribute( AttributeName = "schemaLocation", Namespace = "http://www.w3.org/2001/XMLSchema-instance", Form = XmlSchemaForm.Qualified )]
+        public string SchemaLocation {
+            get => "http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd";
+            set => throw new System.NotImplementedException();
+        }
+
+        [XmlAttribute( AttributeName = "version" )]
+        public string Version {
+            get => "1.1";
+            set => throw new System.NotImplementedException();
         }
     }
 }
--- src/iSki2gpx.Converter/GPX/Writer/GpxWriter.cs
+++ src/iSki2gpx.Converter/GPX/Writer/GpxWriter.cs
@@ -1,19 +1,45 @@
 using System.Text;
 using System.Xml;
+using System.Xml.Schema;
 using System.Xml.Serialization;
 using iSki2gpx.Converter.GPX.Models;
 
 namespace iSki2gpx.Converter.GPX.Writer {
     public class GpxWriter {
-        public async Task WriteToFileAsync( GpxElement gpxElement, string filePath ) {
+        public async Task<bool> WriteToFileAsync( GpxElement gpxElement, string filePath ) {
             XmlSerializer serializer = new XmlSerializer( typeof( GpxElement ) );
             XmlWriterSettings options = new XmlWriterSettings() {
                 Indent = true, Encoding = Encoding.UTF8, NamespaceHandling = NamespaceHandling.OmitDuplicates, Async = true
             };
-            using StreamWriter writer = new StreamWriter( filePath, false, Encoding.UTF8 );
-            using XmlWriter xmlWriter = XmlWriter.Create( writer, options );
-            serializer.Serialize( xmlWriter, gpxElement );
-            await writer.FlushAsync();
+            using MemoryStream stream = new MemoryStream();
+            serializer.Serialize( stream, gpxElement );
+
+            stream.Position = 0;
+            string xmlText = Encoding.UTF8.GetString( stream.ToArray() );
+            
+            XmlDocument xmlDocument = new XmlDocument();
+            XmlReaderSettings xmlReaderSettings = new XmlReaderSettings();
+            xmlReaderSettings.Schemas.Add( "http://www.topografix.com/GPX/1/1", "http://www.topografix.com/GPX/1/1/gpx.xsd" );
+            xmlReaderSettings.ValidationType = ValidationType.Schema;
+            xmlReaderSettings.IgnoreWhitespace = true;
+            
+            using XmlReader reader = XmlReader.Create( stream, xmlReaderSettings );
+            xmlDocument.Load( reader );
+
+            bool result = true;
+            xmlDocument.Validate( ( sender, args ) => {
+                if( args.Severity == XmlSeverityType.Error ) {
+                    result = false;
+                } else if( args.Severity == XmlSeverityType.Warning || args.Exception == null ) {
+                    result = true;
+                }
+            } );
+
+            if( result ) {
+                await File.WriteAllTextAsync( filePath, xmlText, Encoding.UTF8 );
+            }
+
+            return result;
         }
     }
 }
